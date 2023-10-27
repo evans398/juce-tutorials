db8fe63be88a64fae633a4920397ce1f09ad6d90
evans398 <34012709+evans398@users.noreply.github.com>
2023-10-27 12:33:44 +0200

    Initial commit

---
+++ Apps/GuiAppTemplate/Window/Main.cpp
@@ -0,0 +1,31 @@
+#include "MainWindow.h"
+
+namespace GuiApp
+{
+class GuiAppTemplateApplication : public juce::JUCEApplication
+{
+public:
+    const String getApplicationName() override { return JUCE_APPLICATION_NAME_STRING; }
+    const String getApplicationVersion() override { return JUCE_APPLICATION_VERSION_STRING; }
+    bool moreThanOneInstanceAllowed() override { return true; }
+
+    void initialise(const String& commandLine) override
+    {
+        if (!commandLine.contains("console"))
+            mainWindow = std::make_unique<MainWindow>(getApplicationName());
+    }
+
+    void shutdown() override { mainWindow.reset(); }
+
+    void systemRequestedQuit() override { quit(); }
+
+    void anotherInstanceStarted(const String& /*commandLine*/) override {}
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+// This macro generates the main() routine that launches the app.
+START_JUCE_APPLICATION(GuiAppTemplateApplication)
+
+} // namespace GuiApp
