711e29685bccdac2ff895a592bb1398046987119
Nick Evans <nickevans@poblenou-131-15.eduroamcp.upf.edu>
2023-11-15 17:36:40 +0100

    Full implementation of kick snare loop

--- Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
+++ Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
@@ -7,16 +7,42 @@ void MidiFXProcessor::processBlock(juce::AudioBuffer<float>& audioBuffer,
     tempBuffer.clear();
     audioBuffer.clear();
 
-    for (auto m: midiMessages)
-    {
-        auto message = m.getMessage();
-        auto samplePos = m.samplePosition;
+    // get Playhead info && buffer size && sample rate from host
+    auto playHead = getPlayHead();
+    auto positionInfo = playHead->getPosition();
+    auto bpm = positionInfo->getBpm();
+    auto fs = getSampleRate();
+    auto buffSize = audioBuffer.getNumSamples();
 
-        //First, debug all incoming messages to console:
-        std::cout << "Incoming:" << message.getDescription()
-                  << " SamplePos:" << juce::String(samplePos) << std::endl;
-        
-        tempBuffer.addEvent(message, samplePos);
+    if (positionInfo.hasValue() && bpm.hasValue() && positionInfo->getIsPlaying()) {
+        // figure out the end point ppq of buffer using pos, buffer size and sample rate
+        auto bufferLengthPpq = (buffSize) * *bpm / (fs * 60.0);
+        auto pos = positionInfo->getPpqPosition();
+        auto endPos = *pos + bufferLengthPpq;
+        // Note all timings should be modulus of 4 (as we want to loop the 1 bar pattern)
+        auto denominator = 4.0;
+        auto modPosSeq = std::fmod(*pos, denominator);
+        auto modEndPosSeq = std::fmod(endPos, denominator);
+        DBG("Value of PosSeq: " << *pos);
+        DBG("Value of EndPosSeq: " << endPos);
+        // Calculate samples per quarter note
+        double samplesPerQuarterNote = 60.0 / *bpm * fs;
+//
+        for(auto m: midiSequence) {
+            auto message = m->message;
+            double ppqPosition;
+            if ((modPosSeq <= message.getTimeStamp() && message.getTimeStamp() <= modEndPosSeq)) {
+                ppqPosition = message.getTimeStamp() - modPosSeq;
+                int samplePosition = ppqPosition * samplesPerQuarterNote;
+                tempBuffer.addEvent(message, samplePosition);
+                DBG("ADDED MIDI NOTE");
+            } else if (modPosSeq > modEndPosSeq && message.getTimeStamp() == 0) {
+                ppqPosition = denominator - modPosSeq;
+                int samplePosition = ppqPosition * samplesPerQuarterNote;
+                tempBuffer.addEvent(message, samplePosition);
+                DBG("ADDED MIDI NOTE");
+            }
+        }
     }
 
     midiMessages.swapWith(tempBuffer);
--- Plugins/MidiFXTemplate/Source/PluginProcessor.h
+++ Plugins/MidiFXTemplate/Source/PluginProcessor.h
@@ -10,12 +10,37 @@ public:
 
     juce::AudioProcessorEditor* createEditor() override;
 
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override
+    {
+        auto playHead = getPlayHead();
+        auto positionInfo = playHead->getPosition();
+        auto bpm = positionInfo->getBpm();
+        // Initialize the MIDI sequence here, set the tempo, and create the note-on message.
+        midiSequence.clear();
+
+        auto noteOnKickMessage = juce::MidiMessage::noteOn((int)1, (int)36, (float)0.8);
+        auto noteOffKickMessage = juce::MidiMessage::noteOff((int)1, (int)36);
+        auto noteOnSnareMessage = juce::MidiMessage::noteOn((int)1, (int)39, (float)0.8);
+        auto noteOffSnareMessage = juce::MidiMessage::noteOff((int)1, (int)39);
+
+        for (int i = 0; i < 4; ++i)
+        {
+            if (i == 0 || i == 2) {
+                midiSequence.addEvent(noteOnKickMessage, i);
+                midiSequence.addEvent(noteOffKickMessage, i + .001);
+            } else{
+                // Add note-on events at quarter note intervals within a 1-bar loop.
+                midiSequence.addEvent(noteOnSnareMessage, i);
+                midiSequence.addEvent(noteOffSnareMessage, i + .001);
+            }
+        }
+    }
+
 
 private:
 
     juce::MidiBuffer tempBuffer;
 
-    juce::MidiMessageSequence messageSequence{};
-
+    juce::MidiMessageSequence midiSequence{};
 
 };
