12b849e8031f14a40439e531d8c534cc3541acf8
Nick Evans <nickevans@Nicks-MacBook-Pro-2.local>
2023-12-01 23:04:44 +0100

    Adjusting param correctly changes kick density. Next is to add alternating kick/snare

--- Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
+++ Plugins/MidiFXTemplate/Source/PluginProcessor.cpp
@@ -26,11 +26,14 @@ void MidiFXProcessor::processBlock(juce::AudioBuffer<float>& audioBuffer,
 
     // Get the AudioParameterInt by ID
     auto* densityParam = dynamic_cast<juce::AudioParameterInt*>(apvts.getParameter("DENSITY"));
+    int densityParamValue;
 
     if (densityParam != nullptr)
     {
         // Use myIntParameter as needed...
-        int densityParamValue = densityParam->get();
+        densityParamValue = densityParam->get();
+    } else {
+        return;
     }
 
     // get Playhead info && buffer size && sample rate from host
@@ -45,10 +48,11 @@ void MidiFXProcessor::processBlock(juce::AudioBuffer<float>& audioBuffer,
         // figure out the end point ppq of buffer using pos, buffer size and sample rate
         auto bufferLengthPpq = (buffSize) * *bpm / (fs * 60.0);
         auto pos = positionInfo->getPpqPosition();
-        auto endPos = *pos + bufferLengthPpq;
+        auto notePos = *pos * densityParamValue;
+        auto endPos = notePos + (bufferLengthPpq * densityParamValue);
         // Note all timings should be modulus of 4 (as we want to loop the 1 bar pattern)
-        auto denominator = 4.0;
-        auto modPosSeq = std::fmod(*pos, denominator);
+        auto denominator = 4.0 * densityParamValue;
+        auto modPosSeq = std::fmod(notePos, denominator);
         auto modEndPosSeq = std::fmod(endPos, denominator);
 //        DBG("Value of PosSeq: " << *pos);
 //        DBG("Value of EndPosSeq: " << endPos);
@@ -56,22 +60,23 @@ void MidiFXProcessor::processBlock(juce::AudioBuffer<float>& audioBuffer,
         DBG("Value of modEndPosSeq: " << modEndPosSeq);
         // Calculate samples per quarter note
         double samplesPerQuarterNote = 60.0 / *bpm * fs;
+        double samplesPerNote = (60.0 / *bpm * fs) / densityParamValue;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < (4.0 * densityParamValue); i++)
         {
-            double ppqPosition;
+            double notePosition;
             if ((modPosSeq <= i && i <= modEndPosSeq))
             {
-                ppqPosition = i - modPosSeq;
-                int samplePosition = ppqPosition * samplesPerQuarterNote;
+                notePosition = i - modPosSeq;
+                int samplePosition = notePosition * samplesPerNote;
                 noteOnKickMessage.setTimeStamp(0);
                 tempBuffer.addEvent(noteOnKickMessage, samplePosition);
                 DBG("ADDED MIDI NOTE");
             }
             else if (modPosSeq > modEndPosSeq && i == 0)
             {
-                ppqPosition = denominator - modPosSeq;
-                int samplePosition = ppqPosition * samplesPerQuarterNote;
+                notePosition = denominator - modPosSeq;
+                int samplePosition = notePosition * samplesPerQuarterNote;
                 noteOnKickMessage.setTimeStamp(0);
                 tempBuffer.addEvent(noteOnKickMessage, samplePosition);
                 DBG("ADDED MIDI NOTE");
